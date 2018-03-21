a6abaee459f499352ccdb97c6b9388170303d952
muntasir abir <amabirbd@gmail.com>
2018-03-21 13:12:53 +0600

    main python file

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
-"""pixabay_api_key='4119664-75cc2144f4a944b21e461e646'
+"""
 	Aurhor : Abir
 
 """
 import urllib.request
 import python_pixabay
-from bs4 import BeautifulSoup
 import os
 import ctypes
 from random import *
-
+# pixabay_api_key='4119664-75cc2144f4a944b21e461e646'
 pix = python_pixabay.Pixabay('4119664-75cc2144f4a944b21e461e646')
 # custom image search
 cis = pix.image_search(q='',
@@ -17,15 +16,15 @@ cis = pix.image_search(q='',
                        order = 'latest',)
 
 
-#myPath="C:\\Users\\Freeware Sys\\Desktop\\pix"
+# if directory doesn't exist creat directory to save the pictures
 myPath="C:\\pix"
 if not os.path.exists(myPath):
         os.makedirs(myPath)
 
 
 def get_images(num=randint(1,20)):
-    """get image url of the random list indexes"""
-    #print(cis['hits'][i]['largeImageURL'])
+    """get image url of the random list indexes between 1 - 20"""
+    #get the url of the image
     u =cis['hits'][num]['largeImageURL']
     return u
 
@@ -36,15 +35,18 @@ while i<2:
 
     print('Downloading file ')
 
+	#regular expression  to get the name of the file after the "/"
     filename=img_url.split('/')[-1]
+	#joining the path of the downloaded file to th directory
     fullfilename = os.path.join(myPath,filename)
     #print(fullfilename)
     urllib.request.urlretrieve(img_url,fullfilename)
     i+=1
-
+#setting desktop wallpaper for windows
 SPI_SETDESKWALLPAPER = 20
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, fullfilename , 0)
-#print(images)
+
+
 
 
 
