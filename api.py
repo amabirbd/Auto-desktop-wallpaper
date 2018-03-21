79f771d5e6ac3f0c7df99c5d1a197a0083b04b58
muntasir abir <amabirbd@gmail.com>
2018-03-21 13:15:37 +0600

    main python file

--- api.py
+++ api.py
@@ -45,36 +45,3 @@ while i<2:
 #setting desktop wallpaper for windows
 SPI_SETDESKWALLPAPER = 20
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, fullfilename , 0)
-
-
-
-
-
-
-"""
-myPath="C:\\Users\\Freeware Sys\\Desktop\\pix"
-
-fullfilename = os.path.join(myPath, 'filename.jpg')
-urllib.request.urlretrieve(u, fullfilename)
-"""
-
-
-"""
-#after this im using beautiful soup to parse html
-#it's really not necessesary.I can just use lareImageURL
-#to directly download the image.
-
-#ob=urllib.request.urlretrieve(u, "local.html")
-#soup=BeautifulSoup('local.html','html.parser')
-#make a function to do all these
-#refactor
-ob=urllib.request.urlopen(u)
-print(type(ob))
-soup=BeautifulSoup(ob,'html.parser')
-#print(soup.prettify())
-
-all_links=soup.find_all('a')
-
-images = [img for img in soup.findAll('img')]
-print (str(len(images)) + "images found.")
-"""
